2f544d0e5be0aba95b843d9d56ee092e73e4a8fa
Pierre-Emmanuel Daigre <pe.daigre@gmail.com>
2021-05-29 00:03:08 +0200

    add contact model

---
+++ db/migrate/20210528220208_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contacts do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :phone_number
+      t.string :email
+      t.boolean :email_confirmation
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_28_220208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "phone_number"
+    t.string "email"
+    t.boolean "email_confirmation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
