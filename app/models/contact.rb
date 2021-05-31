a2a0a4d02711522a1f959cfd3ad2ccca60fddba0
Pierre-Emmanuel Daigre <pe.daigre@gmail.com>
2021-05-31 12:26:57 +0200

    fix validation in contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-    validates_presence_of :firstname, :lastname, :phone_number, :email, 
+    validates_presence_of :firstname, :lastname, :phone_number, :email 
     validates_length_of :first_name, :last_name, minimum: 2, maximum: 40
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
