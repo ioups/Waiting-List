0c6eb6a3917025adf64c3bd7b6f228f63b184435
Pierre-Emmanuel Daigre <pe.daigre@gmail.com>
2021-06-09 12:16:18 +0200

    modify host in production.rb with www

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = { api_key: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { :host => "http://brest-coworking.xyz" }
+  config.action_mailer.default_url_options = { :host => "http://www.brest-coworking.xyz/" }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
