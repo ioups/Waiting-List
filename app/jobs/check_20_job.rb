11a1e6282bec64a1d2059db04d043140ad6d5866
Pierre-Emmanuel Daigre <pe.daigre@gmail.com>
2021-06-11 13:18:28 +0200

    update check20 job with production values

--- app/jobs/check_20_job.rb
+++ app/jobs/check_20_job.rb
@@ -5,14 +5,14 @@ class Check20Job < ApplicationJob
     contact = Contact.find(contact_id)
     puts "contact found"
     if contact.renewed_on
-      if (DateTime.now.to_i - contact.renewed_on.to_i) / 60 >= 1
+      if (DateTime.now.to_i - contact.renewed_on.to_i) / 86400 >= 20
         contact.optout!
         "puts contact renewed output"
         ContactMailer.optout(contact).deliver_later
         "puts contact renewed mail sent"
       end
     elsif contact.email_confirmation == true
-      if (DateTime.now.to_i - contact.validated_on.to_i) / 60 >= 1 
+      if (DateTime.now.to_i - contact.validated_on.to_i) / 86400 >= 20 
         contact.optout!
         "puts contact confirmed output"
         ContactMailer.optout(contact).deliver_later
